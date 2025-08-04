35efa08710ae13a1cc85bc29b7fe9b09c0fd5d4f
Artem Povolskikh <artem.povolskikh@gmail.com>
2025-08-04 11:55:31 +0300

    feat: improve loader icon

--- chili/components/Icon/icons.tsx
+++ chili/components/Icon/icons.tsx
@@ -51,7 +51,14 @@ export const ICON_PATHS: Record<Icons, React.ReactNode[]> = {
     <path key="1" d="M1 1l22 22" />,
   ],
   [Icons.Loader]: [
-    <circle key="0" cx="12" cy="12" r="10" />,
+    <line key="0" x1="12" y1="2" x2="12" y2="6" />, 
+    <line key="1" x1="12" y1="18" x2="12" y2="22" />, 
+    <line key="2" x1="4.93" y1="4.93" x2="7.76" y2="7.76" />, 
+    <line key="3" x1="16.24" y1="16.24" x2="19.07" y2="19.07" />, 
+    <line key="4" x1="2" y1="12" x2="6" y2="12" />, 
+    <line key="5" x1="18" y1="12" x2="22" y2="12" />, 
+    <line key="6" x1="4.93" y1="19.07" x2="7.76" y2="16.24" />, 
+    <line key="7" x1="16.24" y1="7.76" x2="19.07" y2="4.93" />, 
   ],
   [Icons.MinusSquare]: [
     <rect key="0" x="3" y="3" width="18" height="18" rx="2" ry="2" />,
