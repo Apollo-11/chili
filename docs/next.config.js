8173dd538681e6c25407f141cfc01f3b1e00bbcb
Artem Povolskikh <artem.povolskikh@gmail.com>
2026-03-23 19:12:03 +0300

    Update actions

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@chili'],
+  transpilePackages: ['chili-local'],
   output: 'export',
   images: {
     unoptimized: true,
   },
   basePath: '/chili-ui',
   eslint: { ignoreDuringBuilds: true },
+  webpack: (config) => {
+    // This tells Next.js: "When you see '@chili', look in the parent folder"
+    config.resolve.alias['@chili'] = path.resolve(__dirname, '../');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
