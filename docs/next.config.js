ac2b5b355a1076a4814e465e5dac80f8f010ea74
Artem Povolskikh <artem.povolskikh@gmail.com>
2026-03-23 18:41:35 +0300

    Update nextjs config for docs

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@chili'],
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/chili-ui',
+  eslint: { ignoreDuringBuilds: true },
 };
 
 module.exports = nextConfig;
